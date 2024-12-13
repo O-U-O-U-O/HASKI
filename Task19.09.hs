9bf0191284fd663ba40e9412062ad0dd48babb03
IVY BELLL <cq971037@gmail.com>
2024-12-13 18:48:33 +0700

    Task19.09.hs

--- Task19.09.hs
+++ Task19.09.hs
@@ -1,16 +1,16 @@
 -- first task
 
-fo :: Double -> Double
-fo x = x 
-go :: Double -> Double
-go x = x 
-res_max:: Double -> Double -> Double
-res_max x y = max (fo x) (go y)
+f :: Double -> Double
+f p = p 
+g :: Double -> Double
+g p = p
+res_max :: Double -> Double -> Double
+res_max p1 p2 = max (f p1) (g p2)
 
 -- second tasks
 
-res_exp :: Double -> Double -> Double
-res_exp p f = max (exp p) (fo f)
+--res_exp :: Double -> Double -> Double
+res_exp p = max (exp p) (f p)
 
 
 
