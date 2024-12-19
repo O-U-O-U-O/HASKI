124b57718d69d92234a6abd66a9379f31f54a762
IVY BELLL <cq971037@gmail.com>
2024-12-19 23:46:54 +0700

    Update Task10.10.hs

--- Task10.10.hs
+++ Task10.10.hs
@@ -1,8 +1,8 @@
 --first task
 
-mapl :: (Int -> Int) -> [Int] -> [Int]
+mapl :: (a -> a) -> [a] -> [a]
 mapl f xs = foldl (\a b -> a ++ [f b]) [] xs      -- a - list, b - elem from xs; every time a get result (f b)
-mapr :: (Int -> Int) -> [Int] -> [Int]
+mapr :: (a -> a) -> [a] -> [a]
 mapr f xs = foldr (\a b -> [f a] ++ b) [] xs      -- reverse
 
 
