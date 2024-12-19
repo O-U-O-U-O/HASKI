f42d438087873744a38a04ca5e21e47228e7a358
IVY BELLL <cq971037@gmail.com>
2024-12-19 17:20:01 +0700

    Update Task19.09.hs

--- Task19.09.hs
+++ Task19.09.hs
@@ -1,16 +1,22 @@
 -- first task
 
-f :: Double -> Double
-f p = p 
-g :: Double -> Double
-g p = p
-res_max :: Double -> Double -> Double
-res_max p1 p2 = max (f p1) (g p2)
+res_max :: (Double -> Double) -> (Double -> Double) -> Double -> Double
+res_max f g p = max (f p) (g p)
 
--- second tasks
 
---res_exp :: Double -> Double -> Double
-res_exp p = max (exp p) (f p)
+-- second task
+
+res_exp :: (Double -> Double) -> Double -> Double
+res_exp f p = max (exp p) (f p)
+
+
+-- third task
+
+res_n :: (Double -> Double) -> Double -> Int -> Double
+res_n f p n
+    | n == 0 = p
+    | n < 0 = error "systems-error"
+    | otherwise = res_n f (f p) (n-1) -- n-times f communicate with p
 
 
 
