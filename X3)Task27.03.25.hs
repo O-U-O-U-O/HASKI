e3500b2fb7037cc315667e9944a6f288efd76901
IVY BELLL <cq971037@gmail.com>
2025-04-02 17:49:25 +0700

    Update X3)Task27.03.25.hs

--- X3)Task27.03.25.hs
+++ X3)Task27.03.25.hs
@@ -2,7 +2,11 @@ data MozgoVzriv =
   Lst [Int] | RSt [Int] | Plus [Int] | Minus [Int] | Dot [Int] | Comma [Int] | Rbrack [Int] | Lbrack [Int]
   
 so_yee :: MozgoVzriv -> [Int]
-so_yee (Lst (x:y:xs)) = (y:xs) 
+so_yee (Lst (x:y:xs) = (y:xs)
+so_yee (Rst (xs:y:x) = (xs:y)
+so_yee (Plus (x:xs) = (x+1:xs)
+so_yee (Minus (x:xs) = (x-1:xs)
+so_yee (Dot (x:xs) = (x-1:xs)
 main :: IO ()
 main = do
   let id' = Lst [1,2,3]
