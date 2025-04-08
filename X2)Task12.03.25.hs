268c8b43001ba5da658eeb167fab5e09b9ee3a56
BELL GOLDEN HAAAAND <cq971037@gmail.com>
2025-04-08 11:16:55 +0700

    Update X2)Task12.03.25.hs

--- X2)Task12.03.25.hs
+++ X2)Task12.03.25.hs
@@ -15,15 +15,61 @@ john = Person "John" "Lee" Nothing (Just miranda)
 david = Person "David" "Lee" (Just john) (Just alice)
 
 mf :: Person -> Maybe Person
-mf (david) = Just alice
+mf bro = 
+  case mother bro of  --check bro's mammy 
+  Nothing -> Nothing
+  Just mamula -> case father mamula of   --check mom's parents 
+              Nothing -> Nothing
+              Just ded -> Just ded  --ded is found
 
+fm :: Person -> Maybe Person
+fm bro = 
+   father bro >>= (\_ -> mother bro)
+   
 main :: IO ()
 main = do
-   let m = mf (david)
+   let n = mf (david)
+   let m = fm (david)
+   putStrLn $ show n
    putStrLn $ show m
 
 
+--SECOND
 
+data Person = Person {                 -- Jane Russ        Miranda
+ name :: String,                         -- Alice       John
+ surname :: String,                             --David
+ father :: Maybe Person,
+ mother :: Maybe Person
+} deriving Show
+
+jane = Person "Jane" "Smith" Nothing Nothing
+russ = Person "Russ" "Cox" Nothing Nothing
+miranda = Person "Miranda" "Lee" Nothing Nothing
+alice = Person "Alice" "Cox" (Just russ) (Just jane)
+john = Person "John" "Lee" Nothing (Just miranda)
+david = Person "David" "Lee" (Just john) (Just alice)
+
+hp :: Person -> Maybe Person
+hp bro = 
+  case mother bro of  
+  Nothing -> Nothing
+  Just mamula -> case father bro of    
+              Nothing -> Nothing
+              Just papa -> case mother mamula of
+                        Nothing -> Nothing
+                        Just babkam -> case father mamula of    
+                                   Nothing -> Nothing
+                                   Just dedm -> case mother papa of
+                                             Nothing -> Nothing
+                                             Just babkap -> case father papa of
+                                                         Nothing -> Nothing
+                                                         Just dedp -> Just dedp
+
+main :: IO ()
+main = do
+   let n = hp david
+   putStrLn $ show n
 
 
 
